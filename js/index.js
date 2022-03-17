f03385a8016bea08f368c5122cf6a2c19231c595
philredman <phil.redman@gmail.com>
2022-03-16 21:03:04 -0500

    further attempts - fade doesn't work, even with complete code from 19.3.3

--- js/index.js
+++ js/index.js
@@ -20,29 +20,58 @@
 // });
 
 
-$(".card").on("click", function(){
+// $(".card").on("click", function(){
+//     $(".searchButton").toggleClass("active");
+//     if ($(".searchButton").hasClass("active")) {
+//         $(".searchBar").css("height", "100vh");
+//         $("#searchForm").css("opacity","1");
+//         $(".searchIcon").removeClass("fa-search").addClass("fa-times");
+//     } 
+//     else {
+//         $(".searchBar").css('height', "0vh");
+//         $("#searchForm").css("opacity","0");
+//         $(".searchIcon").removeClass("fa-times").addClass("fa-search");
+//     }
+// });
+
+// this isnt going to be an if-else thing if its just a click and animaiton on loading... but I SHOULD be able to make it fade out after a time. just not sure why it aint doing that yet. //
+
+// I'm thinking about this wrong. It needs to just appear when you transition, like the preload spinner on activity 19.3-3.//
+
+
+
+// $(".toggle").click(function(){
+//     $(this).find(".toggleContainer").toggleClass("active");
+//     $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+// });
+
+// function fade (){
+//     $(".batBun").fadeOut("slow");
+//     setTimeout(fade, 3000);
+// }
+
+function fade() {
+$('.preloader').fadeOut("slow");
+}
+setTimeout(fade, 3000);
+
+$(".searchButton").on("click", function(){
     $(".searchButton").toggleClass("active");
     if ($(".searchButton").hasClass("active")) {
         $(".searchBar").css("height", "100vh");
         $("#searchForm").css("opacity","1");
         $(".searchIcon").removeClass("fa-search").addClass("fa-times");
-    } 
+    }
     else {
         $(".searchBar").css('height', "0vh");
         $("#searchForm").css("opacity","0");
         $(".searchIcon").removeClass("fa-times").addClass("fa-search");
     }
-
-// this isnt going to be an if-else thing if its just a click and animaiton on loading... but I SHOULD be able to make it fade out after a time. just not sure why it aint doing that yet. 
-
-});
-
-$(".toggle").click(function(){
+    });
+    
+    $(".toggle").click(function(){
     $(this).find(".toggleContainer").toggleClass("active");
     $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
-});
-
-function fade (){
-    $(".batBun").fadeOut("slow");
-}
-setTimeout(fade,3000);
+    });
+    
+    
