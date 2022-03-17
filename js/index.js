859e02daef52b75d1fec369bf61afd1d1d061a17
philredman <phil.redman@gmail.com>
2022-03-16 21:36:29 -0500

    it helps if you actually add the jQuery scripts, it turns out.

--- js/index.js
+++ js/index.js
@@ -45,33 +45,7 @@
 //     $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
 // });
 
-// function fade (){
-//     $(".batBun").fadeOut("slow");
-//     setTimeout(fade, 3000);
-// }
-
-function fade() {
-$('.preloader').fadeOut("slow");
+function fade (){
+    $(".batBun").fadeOut("slow");
 }
-setTimeout(fade, 3000);
-
-$(".searchButton").on("click", function(){
-    $(".searchButton").toggleClass("active");
-    if ($(".searchButton").hasClass("active")) {
-        $(".searchBar").css("height", "100vh");
-        $("#searchForm").css("opacity","1");
-        $(".searchIcon").removeClass("fa-search").addClass("fa-times");
-    }
-    else {
-        $(".searchBar").css('height', "0vh");
-        $("#searchForm").css("opacity","0");
-        $(".searchIcon").removeClass("fa-times").addClass("fa-search");
-    }
-    });
-    
-    $(".toggle").click(function(){
-    $(this).find(".toggleContainer").toggleClass("active");
-    $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
-    });
-    
-    
+setTimeout(fade, 550);
