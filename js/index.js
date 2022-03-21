dde174d34699af3920a3f84de25730c974212e21
philredman <phil.redman@gmail.com>
2022-03-20 20:26:00 -0500

    Removed some unnecessary commentary, included jQuery event listener for font change on mouseover, updated the bunspin

--- js/index.js
+++ js/index.js
@@ -1,52 +1,15 @@
-// cannibalized from activity 19.3-3
-
-// $(".searchButton").on("click", function(){
-//     $(".searchButton").toggleClass("active");
-//     if ($(".searchButton").hasClass("active")) {
-//         $(".searchBar").css("height", "100vh");
-//         $("#searchForm").css("opacity","1");
-//         $(".searchIcon").removeClass("fa-search").addClass("fa-times");
-//     } 
-//     else {
-//         $(".searchBar").css('height', "0vh");
-//         $("#searchForm").css("opacity","0");
-//         $(".searchIcon").removeClass("fa-times").addClass("fa-search");
-//     }
-// });
-
-// $(".toggle").click(function(){
-//     $(this).find(".toggleContainer").toggleClass("active");
-//     $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
-// });
-
-
-// $(".card").on("click", function(){
-//     $(".searchButton").toggleClass("active");
-//     if ($(".searchButton").hasClass("active")) {
-//         $(".searchBar").css("height", "100vh");
-//         $("#searchForm").css("opacity","1");
-//         $(".searchIcon").removeClass("fa-search").addClass("fa-times");
-//     } 
-//     else {
-//         $(".searchBar").css('height', "0vh");
-//         $("#searchForm").css("opacity","0");
-//         $(".searchIcon").removeClass("fa-times").addClass("fa-search");
-//     }
-// });
-
-// this isnt going to be an if-else thing if its just a click and animaiton on loading... but I SHOULD be able to make it fade out after a time. just not sure why it aint doing that yet. //
-
-// I'm thinking about this wrong. It needs to just appear when you transition, like the preload spinner on activity 19.3-3.//
-
-
-
-// $(".toggle").click(function(){
-//     $(this).find(".toggleContainer").toggleClass("active");
-//     $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
-// });
-
 function fade (){
     $(".batBun").fadeOut("slow");
 }
-setTimeout(fade, 550);
+setTimeout(fade, 250);
+
+//feels like that's not enough
+
 // change FONT when you hover or tap? taht might be fun. 
+
+$('.navTab, .navLink, button').mouseover(function() {
+    $(this).css("font-family", "'Rye', cursive");
+});
+$('.navTab, .navLink, button').mouseout(function() {
+    $(this).css("font-family", "Rosario, Times, serif");
+});
